f906d0c9da2ddcef4e7758e8dfc049e898ac4bd2
Flo-Schield-Bobby <florent.schildknecht@gmail.com>
2012-11-24 18:46:05 +0100

    Correct header script (clean version)

--- src/FSB/ASTT/FrontBundle/Resources/public/js/header.js
+++ src/FSB/ASTT/FrontBundle/Resources/public/js/header.js
@@ -1,28 +1,39 @@
-$(document).ready(function() {
-    var animationDuration = 1000;
-    var globalDuration = animationDuration * ($('.header-photo', '#header').size() * 2);
+(function ($) {
+    _animationDuration = 1000,
+    _elements = [
+        $('#photo-1', '#header'),
+        $('#photo-2', '#header'),
+        $('#photo-3', '#header'),
+        $('#photo-4', '#header'),
+        $('#photo-5', '#header')
+    ],
+    _animationDescription = [
+        { elt: _elements[0], to: 0.0 },
+        { elt: _elements[1], to: 1.0 },
+        { elt: _elements[1], to: 0.0 },
+        { elt: _elements[2], to: 1.0 },
+        { elt: _elements[2], to: 0.0 },
+        { elt: _elements[3], to: 1.0 },
+        { elt: _elements[3], to: 0.0 },
+        { elt: _elements[4], to: 1.0 },
+        { elt: _elements[4], to: 0.0 },
+        { elt: _elements[0], to: 1.0 },
+    ],
+    _globalDuration = (_elements.length) * 2 * _animationDuration;
+
     launchAnimation();
-    var animation = setInterval(launchAnimation, globalDuration);
-    
-    function launchAnimation() {
-        $('#photo-1', '#header').fadeTo(animationDuration, 0.0, function() {
-            $('#photo-2', '#header').fadeTo(animationDuration, 1.0, function() {
-                $('#photo-2', '#header').fadeTo(animationDuration, 0.0, function() {
-                    $('#photo-3', '#header').fadeTo(animationDuration, 1.0, function() {
-                        $('#photo-3', '#header').fadeTo(animationDuration, 0.0, function() {
-                            $('#photo-4', '#header').fadeTo(animationDuration, 1.0, function() {
-                                $('#photo-4', '#header').fadeTo(animationDuration, 0.0, function() {
-                                    $('#photo-5', '#header').fadeTo(animationDuration, 1.0, function() {
-                                        $('#photo-5', '#header').fadeTo(animationDuration, 0.0, function() {
-                                            $('#photo-1', '#header').fadeTo(animationDuration, 1.0);
-                                        });
-                                    });
-                                });
-                            });
-                        });
-                    });
-                });
-            });
-        });
+    setInterval(launchAnimation, _globalDuration);
+})(jQuery);
+
+function launchAnimation() {
+    for (var i = 0, animationSize = _animationDescription.length; i < animationSize; i++) {
+        var _this = _animationDescription[i];
+        fadeElement(_this, i);
     }
-});
+}
+
+function fadeElement(object, index) {
+    setTimeout(function () {
+        object.elt.fadeTo(_animationDuration, object.to);
+    }, _animationDuration * index);
+}
