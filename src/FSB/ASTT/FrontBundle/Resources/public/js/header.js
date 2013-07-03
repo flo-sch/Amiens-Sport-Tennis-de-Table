b6e504799217b9410e9e412751ee4f71ec26d78f
Flo-Schield-Bobby <florent.schildknecht@gmail.com>
2013-07-03 23:40:59 +0200

    Correct photos slider

--- src/FSB/ASTT/FrontBundle/Resources/public/js/header.js
+++ src/FSB/ASTT/FrontBundle/Resources/public/js/header.js
@@ -1,39 +1,37 @@
 (function ($) {
-    _animationDuration = 1000,
-    _elements = [
-        $('#photo-1', '#header'),
-        $('#photo-2', '#header'),
-        $('#photo-3', '#header'),
-        $('#photo-4', '#header'),
-        $('#photo-5', '#header')
-    ],
-    _animationDescription = [
-        { elt: _elements[0], to: 0.0 },
-        { elt: _elements[1], to: 1.0 },
-        { elt: _elements[1], to: 0.0 },
-        { elt: _elements[2], to: 1.0 },
-        { elt: _elements[2], to: 0.0 },
-        { elt: _elements[3], to: 1.0 },
-        { elt: _elements[3], to: 0.0 },
-        { elt: _elements[4], to: 1.0 },
-        { elt: _elements[4], to: 0.0 },
-        { elt: _elements[0], to: 1.0 },
-    ],
-    _globalDuration = (_elements.length) * 2 * _animationDuration;
+    var _animationDuration = 1000,
+        _elements = [
+            $('#header').find('#photo-1').first(),
+            $('#header').find('#photo-2').first(),
+            $('#header').find('#photo-3').first(),
+            $('#header').find('#photo-4').first(),
+            $('#header').find('#photo-5').first()
+        ],
+        _animationDescription = [
+            { elt: _elements[0], to: 0.0 },
+            { elt: _elements[1], to: 1.0 },
+            { elt: _elements[1], to: 0.0 },
+            { elt: _elements[2], to: 1.0 },
+            { elt: _elements[2], to: 0.0 },
+            { elt: _elements[3], to: 1.0 },
+            { elt: _elements[3], to: 0.0 },
+            { elt: _elements[4], to: 1.0 },
+            { elt: _elements[4], to: 0.0 },
+            { elt: _elements[0], to: 1.0 },
+        ],
+        _globalDuration = (_elements.length) * 2 * _animationDuration,
+        fadeElement = function (object, index) {
+            setTimeout(function () {
+                object.elt.fadeTo(_animationDuration, object.to);
+            }, _animationDuration * index);
+        },
+        launchAnimation = function () {
+            var i;
+            for (i = 0, animationSize = _animationDescription.length; i < animationSize; i++) {
+                fadeElement(_animationDescription[i], i);
+            }
+        };
 
     launchAnimation();
-    setInterval(launchAnimation, _globalDuration);
-})(jQuery);
-
-function launchAnimation() {
-    for (var i = 0, animationSize = _animationDescription.length; i < animationSize; i++) {
-        var _this = _animationDescription[i];
-        fadeElement(_this, i);
-    }
-}
-
-function fadeElement(object, index) {
-    setTimeout(function () {
-        object.elt.fadeTo(_animationDuration, object.to);
-    }, _animationDuration * index);
-}
+    // setInterval(launchAnimation, _globalDuration);
+})(window.jQuery);
